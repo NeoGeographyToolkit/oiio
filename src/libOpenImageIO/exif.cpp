fe50327e9c1ea45d55ce780d0d115d8a3732e3cd
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2023-12-06 13:04:30 -0800

    Fix quircky error

--- src/libOpenImageIO/exif.cpp
+++ src/libOpenImageIO/exif.cpp
@@ -1305,6 +1305,7 @@ encode_exif(const ImageSpec& spec, std::vector<char>& blob)
     if (gpsdirs.size()) {
         // Add some required Exif tags that wouldn't be in the spec
         static char ver[] = { 2, 2, 0, 0 };
+        int GPSTAG_VERSIONID = 0;
         append_tiff_dir_entry(gpsdirs, blob, GPSTAG_VERSIONID, TIFF_BYTE, 4,
                               &ver, tiffstart);
     }
